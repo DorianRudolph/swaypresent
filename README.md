a7025c6500febca7b28263bb2d0502095ca0c1d1
Dorian Rudolph <mail@dorianrudolph.com>
2023-06-12 10:31:44 +0200

    add option to match aspect ratio

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Small script to setup sway for a presentation using wl-mirror.
 ## Usage
 
 ```
-usage: swaypresent.py [-h] [-s SOURCE] [-d DESTINATION] [-w WORKSPACE] [-f]
+usage: swaypresent.py [-h] [-s SOURCE] [-d DESTINATION] [-w WORKSPACE] [-o] [-r]
 
 Launch wl-mirror on a new workspace in fullscreen
 
@@ -17,8 +17,9 @@ options:
                         The output where the mirror is displayed (default: second)
   -w WORKSPACE, --workspace WORKSPACE
                         Name of workspace where mirror will be displayed
-  -f, --fix-overlapping
+  -o, --fix-overlapping
                         Fix overlapping outputs
+  -r, --match-ratio     Set aspect ratio of source output to match destination output
 ```
 
 Once launched, you can select a different region with `wl-present set-region` or output with `wl-present set-output`.
--- swaypresent.py
+++ swaypresent.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python
 
+#    Copyright 2023 Dorian Rudolph
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+
 import i3ipc
 import argparse
 import sys
@@ -24,7 +39,8 @@ def main():
     parser.add_argument('-s', '--source', help='The output to mirror (default: first)')
     parser.add_argument('-d', '--destination', help='The output where the mirror is displayed (default: second)')
     parser.add_argument('-w', '--workspace', default='p', help='Name of workspace where mirror will be displayed')
-    parser.add_argument('-f', '--fix-overlapping', action='store_true', help='Fix overlapping outputs')
+    parser.add_argument('-o', '--fix-overlapping', action='store_true', help='Fix overlapping outputs')
+    parser.add_argument('-r', '--match-ratio', action='store_true', help='Set aspect ratio of source output to match destination output')
     args = parser.parse_args()
 
     i3 = i3ipc.Connection()
@@ -51,9 +67,26 @@ def main():
     src = src[0]
     dst = dst[0]
 
+    if args.match_ratio:
+        w = src.rect.width
+        h = src.rect.height
+        a_src = w / h
+        a_dst = dst.rect.width / dst.rect.height
+        change = False
+        if a_src < a_dst - 0.01:
+            h = int(round(w / a_dst))
+            change = True
+        elif a_src > a_dst + 0.01:
+            w = int(round(h * a_dst))
+            change = True
+        if change:
+            i3.command(f'output {src_output} mode --custom {w}x{h}')
+            src = [o for o in i3.get_outputs() if o.name == src_output][0]
+
     if args.fix_overlapping and check_overlap(src.rect, dst.rect):
         i3.command(f'output {src_output} position '
                    f'{dst.rect.x + (dst.rect.width - src.rect.width) // 2} {dst.rect.y + dst.rect.height}')
+
 
     i3.command('workspace p')
     i3.command(f'move workspace output {dst_output}')
